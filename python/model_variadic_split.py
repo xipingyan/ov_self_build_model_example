23c3da2c2efe266983e59d368b055835c86261fa
xipingya <xiping.yan@intel.com>
2024-06-18 11:15:41 +0800

    update model of variadic split

--- python/model_variadic_split.py
+++ python/model_variadic_split.py
@@ -9,18 +9,24 @@ import time
 import os
 
 def model():
-    data = opset.parameter([6, 2], Type.i32, name = 'data')
-    axis  = op.Constant(Type.i32, Shape([1]), [0])
-    split_length  = op.Constant(Type.i32, Shape([3]), [3,2,1])
-    print("split_length=", split_length)
-   
-    op_variadic_split = opset.variadic_split(data, axis, split_length)
+    # MutMul
+    data = opset.parameter([1, 9], Type.f32, name = 'data')
+    weights = op.Constant(np.full((9, 9), 2.0).astype(np.float32))    
+    matmul = opset.matmul(data, weights, False, False)
+    
+    # VariadicSplit
+    axis  = op.Constant(Type.i32, Shape([1]), [1])
+    split_length  = op.Constant(Type.i32, Shape([3]), [6,2,1])
+    print("split_length=", split_length)   
+    op_variadic_split = opset.variadic_split(matmul, axis, split_length)
     op_variadic_split.set_friendly_name("my_variadic_split")
  
-    reshape1 = opset.reshape(op_variadic_split.output(0), op.Constant(Type.i32, Shape([1]), [6]), special_zero=False)
-    reshape2 = opset.reshape(op_variadic_split.output(1), op.Constant(Type.i32, Shape([1]), [4]), special_zero=False)
-    reshape3 = opset.reshape(op_variadic_split.output(2), op.Constant(Type.i32, Shape([1]), [2]), special_zero=False)
+    # Reshape
+    reshape1 = opset.reshape(op_variadic_split.output(0), ov.Shape([1, 2, 3]), special_zero=False)
+    reshape2 = opset.reshape(op_variadic_split.output(1), ov.Shape([1, 1, 2]), special_zero=False)
+    reshape3 = opset.reshape(op_variadic_split.output(2), ov.Shape([1, 1, 1]), special_zero=False)
 
+    # Result
     Result1 = opset.result(reshape1, name='my_output1')
     Result2 = opset.result(reshape2, name='my_output2')
     Result3 = opset.result(reshape3, name='my_output3')
@@ -33,7 +39,7 @@ def test_variadic_split():
 
     compiled_model = core.compile_model(model=m, device_name="CPU")
 
-    input=np.array([[1, 2],[2, 2],[3, 2],[4, 2],[5, 2],[6, 2]]).astype(np.float32)
+    input=np.array([[1,2,3,4,5,6,7,8,9]]).astype(np.float32)
     result = compiled_model(input)[compiled_model.output(0)]
 
     print("indices=", input)
