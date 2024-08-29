7a71dd07052b2947042ee7db855ed5bd8e63b1e2
xipingya <xiping.yan@intel.com>
2024-08-29 08:10:31 +0000

    if node, add multiple param, first param must be used

--- python/model_if.py
+++ python/model_if.py
@@ -26,36 +26,44 @@ import os
 #               Output
 def model_if():
     input1 = opset.parameter([1], Type.i32, "input1")
+    input_for_then = opset.parameter([1], Type.i32, "input_for_then")
+    input_for_else = opset.parameter([1], Type.i32, "input_for_else")
 
     const1 = op.Constant(np.full((1), -1).astype(np.int32))
     add1 = opset.add(input1, const1)
     add1.set_friendly_name("matmul_input1_const1")
 
-    # mutmal = opset.add(input1, op.Constant(np.full((1), 2).astype(np.int32)))
-    # equal = ov.opset8.equal(add1.output(0), op.Constant(np.full((1), 2).astype(np.int32)))
-    if_op = ov.opset8.if_op()
+    equal = ov.opset8.equal(add1.output(0), op.Constant(np.full((1), 2).astype(np.int32)))
+    if_op = ov.opset8.if_op(equal)
 
+    input_then_if = ov.opset8.parameter([1], np.int32, "input_then_if")
     input_then = ov.opset8.parameter([1], np.int32, "input_then")
-    multiply1 = opset.multiply(input_then, op.Constant(np.full((1), 2).astype(np.int32)))
+    add_then = opset.add(opset.convert(input_then_if, ov.Type.i32), input_then)
+    multiply1 = opset.multiply(add_then, op.Constant(np.full((1), 2).astype(np.int32)))
     multiply1.set_friendly_name("multiply1")
     then_op_result = ov.opset6.result(multiply1, "res_then")
-    body_then_function = Model(results=[then_op_result], parameters=[input_then], name='model_then_body')
+    body_then_function = Model(results=[then_op_result], parameters=[input_then_if, input_then], name='model_then_body')
 
+    input_else_if = ov.opset8.parameter([1], np.int32, "input_else_if")
     input_else = ov.opset8.parameter([1], np.int32, "input_else")
-    multiply2 = opset.multiply(input_else, op.Constant(np.full((1), 4).astype(np.int32)))
+    add_else = opset.add(opset.convert(input_else_if, ov.Type.i32), input_else)
+    multiply2 = opset.multiply(add_else, op.Constant(np.full((1), 4).astype(np.int32)))
     multiply2.set_friendly_name("multiply2")
     else_op_result = ov.opset6.result(multiply2, "res_else")
-    body_else_function = Model(results=[else_op_result], parameters=[input_else], name='body_else_function')
+    body_else_function = Model(results=[else_op_result], parameters=[input_else_if, input_else], name='body_else_function')
 
     if_op.set_then_body(body_then_function)
     if_op.set_else_body(body_else_function)
-    if_op.set_input(add1.output(0), input_then, input_else)
+
+    if_op.set_input(equal.output(0), input_then_if, input_else_if)
+    if_op.set_input(input_for_then.output(0), input_then, None)
+    if_op.set_input(input_for_else.output(0), None, input_else)
 
     result_if = if_op.set_output(then_op_result, else_op_result)
 
     add = opset.add(result_if, op.Constant(np.full((1), 5).astype(np.int32)))
     res = ov.opset6.result(add, "res")
-    return Model(results=[res], parameters=[input1], name='model_if')
+    return Model(results=[res], parameters=[input1, input_for_then, input_for_else], name='model_if')
 
 def test_model_if(device:str):
     print(f'ov version:{ov.get_version()}')
@@ -63,11 +71,13 @@ def test_model_if(device:str):
     model = model_if()
     compiled_model = core.compile_model(model=model, device_name=device)
 
-    input1=np.array([1]).astype(np.float32)
+    input1=np.array([3]).astype(np.float32)
+    input_for_then=np.array([5]).astype(np.int32)
+    input_for_else=np.array([10]).astype(np.int32)
     infer_request = compiled_model.create_infer_request()
 
     print(f"== Run model_if, device={device}")
-    result = infer_request.infer([input1])[compiled_model.output(0)]
+    result = infer_request.infer([input1, input_for_then, input_for_else])[compiled_model.output(0)]
     print(f'== reuslt={result}')
 
 # == Test ReadValue's input is parameter
--- python/test.sh
+++ python/test.sh
@@ -30,4 +30,7 @@ fi
 # numactl -C 0-47 $DEBUG_GDB python model_gather_embedding.py
 # numactl -C 0-47 $DEBUG_GDB python3 ./compare_result_and_expected.py
 # numactl -C 0-47 $DEBUG_GDB python model_gather_embedding_versa.py
-numactl -C 0-47 $DEBUG_GDB python model_stateful.py
+
+# numactl -C 0-47 $DEBUG_GDB python model_stateful.py
+
+OV_CPU_EXEC_GRAPH_PATH=xxx.xml numactl -C 0-47 $DEBUG_GDB python model_if.py
