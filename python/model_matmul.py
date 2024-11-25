bdd09a45610dc5933d474bfda6cf71de3e128c18
xipingyan <xiping.yan@intel.com>
2024-11-25 17:16:20 +0800

    code refacetor matmul test

--- python/model_matmul.py
+++ python/model_matmul.py
@@ -2,18 +2,24 @@ from openvino.runtime import Core, Model, Tensor, PartialShape, Type, Shape, op,
 from openvino.runtime.op import util as op_util
 from openvino.runtime import opset8 as opset
 from openvino.runtime.passes import Manager
+import openvino.runtime as ov
 import numpy as np
 import os
- 
-def const(shape, max=1, min=0):
+
+
+def const(shape):
+    # default generate data with range [0, 1)
     w = np.random.rand(*shape).astype(np.float32)
     return op.Constant(w)
- 
+
+
 def value(*shape):
     return np.random.rand(*shape).astype(np.float32)
 
+
 def new_const_dim(val):
     return op.Constant(Type.i64, Shape([len(val)]), val)
+
 
 def get_const_weight(shape):
     weight_fn = "weight.npy"
@@ -40,6 +46,8 @@ def get_const_weight(shape):
 #                   Add[?,?,384]
 #                    |
 #                 Result
+
+
 def model():
     # input = opset.parameter([1, 6, -1, 64], Type.f32, name='input')
     input = opset.parameter([1, -1, 2], Type.f32, name='input')
@@ -57,38 +65,42 @@ def model():
     Result = opset.result(matmul, name='model_mm_result')
     return Model([Result], [input], 'Model_MM')
 
-ov_device = os.getenv("OV_DEVICE")
-if ov_device is None:
-    print("== Not set device ENV: OV_DEVICE, default adopt 'CPU'.")
-    ov_device = 'CPU'
-print("== Test device is: ", ov_device)
+def main():
+    print("== OpenVINO Version:", ov.get_version())
+    ov_device = os.getenv("OV_DEVICE")
+    if ov_device is None:
+        print("== Not set device ENV: OV_DEVICE, default adopt 'CPU'.")
+        ov_device = 'CPU'
+    print("== Test device is: ", ov_device)
 
-core = Core()
-compiled_model = core.compile_model(model=model(), device_name=ov_device)
-compiled_model_ref = core.compile_model(model=model(), device_name='TEMPLATE')
+    core = Core()
+    compiled_model = core.compile_model(model=model(), device_name=ov_device)
+    compiled_model_ref = core.compile_model(model=model(), device_name='TEMPLATE')
 
-irq = compiled_model.create_infer_request()
-irq_ref = compiled_model_ref.create_infer_request()
+    irq = compiled_model.create_infer_request()
+    irq_ref = compiled_model_ref.create_infer_request()
 
-input_fn = "input.npy"
-if not os.path.exists(input_fn):
-    # Range[0,1)
-    # input = [value(1, 6, 1500, 64)]
-    input = value(1, 5, 2)
-    with open(input_fn, 'wb') as f:
-        np.save(f, input)
+    input_fn = "input.npy"
+    if not os.path.exists(input_fn):
+        # Range[0,1)
+        # input = [value(1, 6, 1500, 64)]
+        input = value(1, 5, 2)
+        with open(input_fn, 'wb') as f:
+            np.save(f, input)
+            f.close()
+    with open(input_fn, 'rb') as f:
+        input = np.load(f)
         f.close()
-with open(input_fn, 'rb') as f:
-    input = np.load(f)
-    f.close()
 
-print("== input: ", input)
+    # print("== input: ", input)
 
-result = irq.infer(input)[compiled_model.output(0)]
-result_ref = irq_ref.infer(input)[compiled_model_ref.output(0)]
+    result = irq.infer(input)[compiled_model.output(0)]
+    result_ref = irq_ref.infer(input)[compiled_model_ref.output(0)]
 
-print('Result shape:', result.shape)
-print('Comare with reference:', 
-      np.allclose(result.data.tolist(), result_ref.data.tolist(), 0.001, 0.001, False))
-print("result=",result.data.tolist())
-print("result_ref=",result_ref.data.tolist())
+    print('== Result shape:', result.shape)
+    print('== Result and Reference are',
+        'same. Success.' if np.allclose(result.data.tolist(), result_ref.data.tolist(), 0.001, 0.001, False) else 'different. Fail.')
+    # print("result=",result.data.tolist())
+    # print("result_ref=",result_ref.data.tolist())
+
+main()
