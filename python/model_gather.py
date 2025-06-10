fe63dbcbff952d3f697c0c3e2b23028efa7efe25
xiping.yan <xiping.yan@intel.com>
2025-06-10 14:01:22 +0800

    gather update again

--- python/model_gather.py
+++ python/model_gather.py
@@ -2,51 +2,59 @@
 import numpy as np
 
 import openvino as ov
-from openvino.runtime import Core, Model, Type, Shape, op
-from openvino.runtime import opset8 as opset
+from openvino import Core, Model, Type, Shape, op
+from openvino import opset8 as opset
 
 def new_const_1_dim(val):
     return op.Constant(Type.i32, Shape([1]), [val])
 
 def model(FeaDim):
+    # shape=[batch, seq, fea]
     input_ids = opset.parameter([-1, -1, FeaDim], Type.i32, name = 'input')
     input_ids_shape = opset.shape_of(input_ids)
 
-    indics = opset.gather(input_ids_shape, new_const_1_dim(1), new_const_1_dim(0))
+    # Get middle value from [-1,-1,FeaDim], for example: get 2 from shape[1,2,3]
+    last_seq_id = opset.gather(input_ids_shape, new_const_1_dim(1), new_const_1_dim(0))
+    # idx start from 0, so need to reduce 1.
+    indics = opset.add(last_seq_id, op.Constant(Type.i64, Shape([1]), [-1]))
 
-    op_gather = opset.gather(input_ids, indics, new_const_1_dim(1))
+    # Get last seq fea, same to input_ids[:,-1,:]
+    last_logits = opset.gather(input_ids, indics, new_const_1_dim(1))
 
-    reshape = opset.reshape(op_gather, opset.constant([-1, FeaDim], dtype=ov.Type.i64), special_zero=True)
+    # Reshape from [-1, 1, FeaDim] to [-1, FeaDim]
+    reshape = opset.reshape(last_logits, opset.constant([-1, FeaDim], dtype=ov.Type.i64), special_zero=True)
  
     Result = opset.result(reshape, name='output')
     return Model([Result], [input_ids], 'model_gather')
 
 def test_gather():
     core = Core()
-    FeaDim = 3
+    FeaDim = 4
     m = model(FeaDim=FeaDim)
 
     # Save model
-    ov.save_model(m, "./tmp_model_gather.xml")
+    # ov.save_model(m, "./tmp_model_gather.xml")
 
     compiled_model = core.compile_model(model=m, device_name="CPU")
     ireq = compiled_model.create_infer_request()
 
-    # input = np.array([[1], [4]]).astype(np.int32)
-    input = np.array([[[111, 112, 113], [121, 122, 123], [131, 132, 133]], 
-                      [[211, 212, 213], [221, 222, 223], [231, 232, 233]]]).astype(np.int32)
+    # input.shape= (2, 3, 4)
+    input = np.array([[[111, 112, 113, 4], [121, 122, 123, 4], [131, 132, 133, 4]], 
+                      [[211, 212, 213, 4], [221, 222, 223, 4], [231, 232, 233, 4]]]).astype(np.int32)
     assert(FeaDim == input.shape[-1])
     
     print("== input.shape=", input.shape)
     print("== input[:,-1,:].shape=", input[:,-1, :].shape)
-    print("== Expected result = input[:,-1,:] =\n", input[:,-1, :])
+    expected_result = input[:,-1, :]
+    print("== Expected result = input[:,-1,:] =\n", expected_result)
 
     # result = compiled_model(input)[compiled_model.output(0)] # or bellow.
-    result = ireq.infer(input)
-    print(result)
-    # print("---------------------->")
-    # print("== result.shape=", result.shape)
-    # print("== OV result =\n", result)
+    ov_result = ireq.infer(input)['output']
+    print("---------------------->")
+    print("== result.shape=", ov_result.shape)
+    print("== OV result =\n", ov_result)
+
+    print("== Expected vs OV result:", (expected_result-ov_result.tolist() < 0.0001).all())
 
 if __name__ == "__main__":
     test_gather()
