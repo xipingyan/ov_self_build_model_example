821ba70f726cee405b939b2ce22ee084a839d1f5
xiping.yan <xiping.yan@intel.com>
2025-06-26 22:34:10 +0800

    enable load data

--- python/pattern_add_add_conv.py
+++ python/pattern_add_add_conv.py
@@ -4,6 +4,7 @@ from openvino.runtime import opset8 as opset
 from openvino.runtime.passes import Manager
 import numpy as np
 import os
+from utils.load_dump_file import load_dump_file
  
 def const(shape):
     w = np.random.uniform(low=-1, high=1.0, size=shape).astype(np.float32)
@@ -12,7 +13,7 @@ def const(shape):
 def value(*shape):
     return np.random.rand(*shape).astype(np.float32)
  
-def model_add_add_conv():
+def model_add_add_conv(weight):
     input1 = opset.parameter([1, 256, 32, 32], Type.f32, name = 'in1')
     input2 = opset.parameter([1, 256, 32, 32], Type.f32, name = 'in2')
 
@@ -31,14 +32,36 @@ def model_add_add_conv():
     Result = opset.result(conv, name='output')
     return Model([Result], [input1,input2], 'Model15')
 
+def model_conv_add(weight):
+    input1 = opset.parameter([1, 256, 32, 32], Type.f32, name = 'in1')
+    input2 = opset.parameter([1, 1024, 1, 1], Type.f32, name = 'in1')
+
+    strides = [1, 1]
+    pads_begin = [0, 0]
+    pads_end = [0, 0]
+    dilations = [1, 1]
+    conv = opset.convolution(input1, weight, strides, pads_begin, pads_end, dilations)
+    
+    add2 = opset.add(conv, input2)
+
+    Result = opset.result(conv, name='output')
+    return Model([Result], [input1,input2], 'Model15')
+
 if __name__ == "__main__":
     core = Core()
-    m = model_add_add_conv()
+    if 0:
+        m = model_add_add_conv()
+        input1 = value(1, 256, 32, 32)
+        input2 = value(1, 256, 32, 32)
+    else:
+        input_root = "./test_data/program1_network1_0_convolution__backbone_stages.3_op_list.2_main_inverted_conv_conv_Conv_WithoutBiases"
+        input1 = load_dump_file(input_root+"_src0.txt", shape_dim=4)
+        weight = load_dump_file(input_root+"_src1.txt", shape_dim=4)
+        input2 = load_dump_file(input_root+"_src2.txt", shape_dim=4)
+        m = model_conv_add(weight)
+
     cm_cpu = core.compile_model(model=m, device_name="CPU")
     cm_gpu = core.compile_model(model=m, device_name="GPU")
-    
-    input1 = value(1, 256, 32, 32)
-    input2 = value(1, 256, 32, 32)
 
     result_cpu = cm_cpu([input1, input2])[0]
     result_gpu = cm_gpu([input1, input2])[0]
---
+++ python/utils/load_dump_file.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def load_dump_file(fn:str, new_shape=None, shape_dim=1):
+    # Read file
+    with open(fn, "r") as file:
+        first_line = file.readline()
+        # parse shape: shape: [b:1, f:256, x:32, y:32, z:1, w:1, u:1, v:1, g:1]
+        first_line = first_line.split(']')[0].split('[')[1]
+        first_line = first_line.split(',')
+        shape = []
+        for itm in first_line:
+            shape.append(int(itm.split(':')[1]))
+        print(" == shape =", shape)
+
+        dt_list = file.readlines()
+        converted_list = [float(item.strip()) for item in dt_list]
+        arr = np.array(converted_list, dtype=np.float32)
+        if new_shape != None:
+            arr = arr.reshape(new_shape)
+        elif shape_dim < len(shape):
+            arr = arr.reshape(shape[:shape_dim])
+        return arr
+
+if __name__ == "__main__":
+    input_root = "./test_data/program1_network1_0_convolution__backbone_stages.3_op_list.2_main_inverted_conv_conv_Conv_WithoutBiases"
+    
+    inp_1 = load_dump_file(input_root + "_src0.txt", shape_dim=4)
+    print(inp_1.shape)
