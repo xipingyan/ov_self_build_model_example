04b387e084a180a4263e360c51ee273176d4fb7a
xipingya <xiping.yan@intel.com>
2025-08-07 13:56:11 +0800

    test model if for mulitiple inputs

--- python/model_if.py
+++ python/model_if.py
@@ -69,10 +69,84 @@ def test_model_if(device:str, then_branch=True):
     result = infer_request.infer([input1, input_for_then, input_for_else])[compiled_model.output(0)]
     print(f'== reuslt = {result}, expected = {input_for_then*2 if then_branch else input_for_else*4}')
 
+
+# ======== Model graph ========
+#              input1
+#                 |
+# input2/input3  If   input4/input5
+#    |          /   \      |
+#  then(multiply1)  else(multiply2)
+#               \   /
+#                Add
+#                 |
+#               Output
+def model_if_multiple_inputs():
+    input1 = opset.parameter([1], Type.i32, "input1")
+    input_for_then_1 = opset.parameter([1], Type.i32, "input_for_then_1")
+    input_for_then_2 = opset.parameter([1], Type.i32, "input_for_then_2")
+    input_for_else_1 = opset.parameter([1], Type.i32, "input_for_else_1")
+    input_for_else_2 = opset.parameter([1], Type.i32, "input_for_else_2")
+
+    if_op = ov.opset8.if_op()
+    # then:
+    input_then_1 = ov.opset8.parameter([1], np.int32, "input_then_1")
+    input_then_2 = ov.opset8.parameter([1], np.int32, "input_then_2")
+    multiply1 = opset.multiply(input_then_1, op.Constant(np.full((1), 2).astype(np.int32)))
+    multiply1.set_friendly_name("multiply1")
+    add = opset.add(multiply1, input_then_2)
+    then_op_result = ov.opset6.result(add, "res_then")
+    body_then_function = Model(results=[then_op_result], parameters=[input_then_1, input_then_2], name='model_then_body')
+
+    # else:
+    input_else_1 = ov.opset8.parameter([1], np.int32, "input_else_1")
+    input_else_2 = ov.opset8.parameter([1], np.int32, "input_else_2")
+    multiply_1 = opset.multiply(input_else_1, op.Constant(np.full((1), 4).astype(np.int32)))
+    multiply_1.set_friendly_name("multiply2")
+    add_1 = opset.add(multiply_1, input_else_2)
+    else_op_result = ov.opset6.result(add_1, "res_else")
+    body_else_function = Model(results=[else_op_result], parameters=[input_else_1, input_else_2], name='body_else_function')
+
+    if_op.set_then_body(body_then_function)
+    if_op.set_else_body(body_else_function)
+
+    if_op.set_input(input1.output(0), input_then_1, input_else_1)
+    if_op.set_input(input1.output(0), input_then_2, input_else_2)
+    if_op.set_input(input_for_then_1.output(0), input_then_1, None)
+    if_op.set_input(input_for_then_2.output(0), input_then_2, None)
+    if_op.set_input(input_for_else_1.output(0), None, input_else_1)
+    if_op.set_input(input_for_else_2.output(0), None, input_else_2)
+
+    result_if = if_op.set_output(then_op_result, else_op_result)
+
+    add = opset.add(result_if, op.Constant(np.full((1), 0).astype(np.int32)))
+    res = ov.opset6.result(add, "res")
+    return Model(results=[res], parameters=[input1, input_for_then_1, input_for_then_2, input_for_else_1, input_for_else_2], name='model_if')
+
+def test_model_if_multiple_inputs(device:str, then_branch=True):
+    print(f'== test_model_if_multiple_inputs device = {device}, then_branch = {then_branch}')
+    core = Core()
+    model = model_if_multiple_inputs()
+    compiled_model = core.compile_model(model=model, device_name=device)
+    
+    input1=np.array([1] if then_branch else [0]).astype(np.float32)
+
+    input_for_then_1=np.array([2]).astype(np.int32)
+    input_for_then_2=np.array([3]).astype(np.int32)
+    input_for_else_1=np.array([4]).astype(np.int32)
+    input_for_else_2=np.array([3]).astype(np.int32)
+    infer_request = compiled_model.create_infer_request()
+
+    print(f"== Run model_if, device={device}")
+    result = infer_request.infer([input1, input_for_then_1, input_for_then_2, input_for_else_1, input_for_else_2])[compiled_model.output(0)]
+    print(f'== reuslt = {result}, expected = {input_for_then_1*2+3 if then_branch else input_for_else_1*4+3}')
+
 # == Test ReadValue's input is parameter
 if __name__ == "__main__":
     print(f'ov version:{ov.get_version()}')
     # test_model_if('TEMPLATE')
     # test_model_if('CPU')
-    test_model_if('GPU', then_branch=True)
-    test_model_if('GPU', then_branch=False)
+    # test_model_if('GPU', then_branch=True)
+    # test_model_if('GPU', then_branch=False)
+    test_model_if_multiple_inputs('GPU', then_branch=True)
+    test_model_if_multiple_inputs('GPU', then_branch=False)
+    
