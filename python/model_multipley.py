99456c147a397c1d113d1d91f294f1bcbe980f96
xiping.yan <xiping.yan@intel.com>
2025-10-09 17:57:41 +0800

    python test pass

--- python/model_multipley.py
+++ python/model_multipley.py
@@ -1,8 +1,6 @@
-from openvino.runtime import Core, Model, Tensor, PartialShape, Type, Shape, op, serialize
-from openvino.runtime.op import util as op_util
 from openvino.runtime import opset8 as opset
-from openvino.runtime.passes import Manager
-import openvino.runtime as ov
+import openvino as ov
+from openvino.runtime import Core, Type, op, Shape, Model
 import numpy as np
 import os
 
@@ -20,7 +18,7 @@ def value(*shape):
 def new_const_dim(val):
     return op.Constant(Type.i64, Shape([len(val)]), val)
 
-def prepare_input(input_shape):
+def prepare_input(input_shape, type=np.float32):
     input_fn = "input.npy"
     if os.path.exists(input_fn):
         with open(input_fn, 'rb') as f:
@@ -32,7 +30,10 @@ def prepare_input(input_shape):
             return input
 
     # Generate new weights ans save
-    new_input = np.random.rand(*input_shape).astype(np.float32)
+    new_input = np.random.rand(*input_shape)*255.0
+    if type is np.uint8:
+        new_input = new_input.astype(type)
+
     with open(input_fn, 'wb') as f:
         np.save(f, new_input)
         f.close()
@@ -40,13 +41,35 @@ def prepare_input(input_shape):
 
 def model_multiply():
     input = opset.parameter([-1, -1, -1, -1], Type.f32, name='input')
-
     scale = op.Constant(Type.f32, Shape([1, 3, 1, 1]), [1.0, 1.0, 1.0])
     matmul = opset.multiply(input, scale)
-
     Result = opset.result(matmul, name='model_multiply_result')
-
     return Model([Result], [input], 'Model_multiply')
+
+def model_multiply_complex_case():
+    raw_images_1 = opset.parameter([-1, -1, -1, -1], Type.u8, name='input1')
+    raw_images_2 = opset.parameter([-1, -1, -1, -1], Type.u8, name='input2')
+    resize_shape = opset.parameter([2], Type.i64, name='input3')
+
+    mean = op.Constant(Type.f32, Shape([1, 3, 1, 1]), [0.0, 0., 0.])
+    scale = op.Constant(Type.f32, Shape([1, 3, 1, 1]), [1.0, 1.0, 1.0])
+
+    raw_images_1_cvt = opset.convert(raw_images_1, Type.f32)
+    img_trans_1 = opset.transpose(raw_images_1_cvt, op.Constant(Type.i32, Shape([4]), [0, 3, 1, 2]))
+    clamp_1 = opset.clamp(img_trans_1, 0, 255)
+    substract_1 = opset.subtract(clamp_1, mean)
+    multiply_1 = opset.multiply(substract_1, scale)
+
+    raw_images_2_cvt = opset.convert(raw_images_2, Type.f32)
+    img_trans_2 = opset.transpose(raw_images_2_cvt, op.Constant(Type.i32, Shape([4]), [0, 3, 1, 2]))
+    clamp_2 = opset.clamp(img_trans_2, 0, 255)
+    substract_2 = opset.subtract(clamp_2, mean)
+    multiply_2 = opset.multiply(substract_2, scale)
+
+    concat = opset.concat({multiply_1, multiply_2}, axis=0)
+
+    result_1 = opset.result(concat, name='multiply_1')
+    return Model([result_1], [raw_images_1, raw_images_2], 'Model_multiply')
 
 def main():
     print("== OpenVINO Version:", ov.get_version())
@@ -57,15 +80,17 @@ def main():
     print("== Test device is: ", ov_device)
 
     core = Core()
-    model = model_multiply()
+    # model = model_multiply()
+    model = model_multiply_complex_case()
     compiled_model = core.compile_model(model=model, device_name=ov_device)
     irq = compiled_model.create_infer_request()
 
     # Ready input:
-    input = prepare_input([1, 3, 2, 2])
+    # input = prepare_input([1, 3, 2, 2])
+    input = prepare_input([1, 2, 2, 3], type=np.uint8)
     # print("== input: ", input)
 
-    result = irq.infer(input)[compiled_model.output(0)]
+    result = irq.infer([input, input])[compiled_model.output(0)]
 
     print('===========================================')
     print('== Result shape:', result.shape)
