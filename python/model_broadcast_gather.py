79454090c8462f22acb51c81b140d6232ff63c14
xipingya <xiping.yan@intel.com>
2026-03-24 09:22:34 +0800

    commented

--- python/model_broadcast_gather.py
+++ python/model_broadcast_gather.py
@@ -8,21 +8,24 @@ def func_broadcast_constant_c(input_tensor, c_value):
     # 1. 获取输入 Tensor 的实际形状 (ShapeOf)
     input_shape = ops.shape_of(input_tensor)
 
-    # 2. 提取第一维 (Batch Size)
+    # 2. 提取第一维 (Batch Size)->shape[1]
     batch_dim = ops.gather(
         data=input_shape, 
         indices=ops.constant([0], dtype=np.int64), 
         axis=ops.constant([0], dtype=np.int64)
     )
+    print("batch_dim=", batch_dim)
 
     # 3. 构建目标形状：[batch_size, 1]
     const_one = ops.constant([1], dtype=np.int64)
+    # Shape = [2], value=[batch_size, 1]
     target_shape = ops.concat([batch_dim, const_one], axis=0)
+    print("target_shape=", target_shape)
 
     # 4. 准备常量 c 并在目标形状上进行广播
     c_node = ops.constant(c_value, dtype=np.float32)
-    result = ops.broadcast(c_node, target_shape)
-
+    result = ops.broadcast(c_node, target_shape) # Shape = [-1, 1], value=[[c_value], [c_value], ...]
+    print("result=", result)
     return result
 
 def create_broadcast_model(c_value):
